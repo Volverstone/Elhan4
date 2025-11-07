30bf1818decb1ac65c8c5908d3c3084a6278ae56
Volverstone <tilek37@mail.ru>
2025-11-07 15:36:24 +0600

    hw2

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,9 @@ from myapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/text/', views.text_response, name='text_response'),
-    path('api/html/', views.html_response, name='html_response'),
+    path('', views.home, name='home'),
+    path('api/text/', views.text_response),
+    path('api/html/', views.html_response),
+    path('posts/', views.post_list, name='post_list'),
+    path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,22 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 def text_response(request):
     return HttpResponse("Привет! Это текстовый ответ от Django API :)")
 
 def html_response(request):
     context = {"title": "Пример шаблона", "message": "Это HTML-страница из шаблона"}
-    return render(request, "myapp/example.html", context)
+    return render(request, "example.html", context)
+
+def home(request):
+    return render(request, "base.html")
+
+
+def post_list(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'myapp/post_list.html', {'posts': posts})
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    return render(request, 'myapp/post_detail.html', {'post': post})
